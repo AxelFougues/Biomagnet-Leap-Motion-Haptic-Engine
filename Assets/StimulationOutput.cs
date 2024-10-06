44f63bbf8f0181661cfcea3fd211ca8adc82bf3f
unknown <azfougues@gmail.com>
2024-10-06 11:57:49 -0400

    Add collision smoothing

--- Assets/StimulationOutput.cs
+++ Assets/StimulationOutput.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 [Serializable]
 public class StimulationOutput : MonoBehaviour {
 
+    const int COLLISION_SMOOTHING_BUFFER = 3;
+
     [Header("Output")]
     [Space]
 
@@ -34,9 +36,11 @@ public class StimulationOutput : MonoBehaviour {
     public Collider surface;
     [HideInInspector]
     public OutputUI outputUI;
+
     
     //Current points of contact with touchable objects
     Dictionary<int, ContactParameters> activeContacts = new Dictionary<int, ContactParameters>();
+    Dictionary<int, RollingAverageFilter> triggerFilters = new Dictionary<int, RollingAverageFilter>();
 
     //Gets set by UI when matched
     public void setUI(OutputUI outputUI) {
@@ -52,110 +56,87 @@ public class StimulationOutput : MonoBehaviour {
     private void OnTriggerEnter(Collider other) {
         int id = other.GetInstanceID();
 
-        if (activeContacts.ContainsKey(id)) return;
-
+        if (activeContacts.ContainsKey(id)) {
+            activeContacts[id].collidesDuringFrame = true;
+            return;
+        }
         Touchable t = other.GetComponent<Touchable>();
         if (t != null) {
             ContactParameters cp = new ContactParameters(surface.transform.position, t, this);
+            cp.collidesDuringFrame = true;
             activeContacts.Add(id, cp);
             t.onContactStart();
+            triggerFilters.Add(id, new RollingAverageFilter(COLLISION_SMOOTHING_BUFFER));
         }
         
     }
 
     private void FixedUpdate() {
-        foreach (ContactParameters cp in activeContacts.Values) {
-            Vector3 direction; float distance; Vector3 velocity;
+        List<int> lost = new List<int>();
 
-            //interaction calculation
-            if (Physics.ComputePenetration(cp.touchable.touchCollider, cp.touchable.transform.position, cp.touchable.transform.rotation, surface, surface.transform.position, surface.transform.rotation, out direction, out distance)) {
+        foreach (int id in activeContacts.Keys) {
+            ContactParameters cp = activeContacts[id];
+            if (Mathf.RoundToInt(triggerFilters[id].getValue(cp.collidesDuringFrame ? 1 : 0)) == 0) {
+                lost.Add(id);
+            } else {
 
-                //outputs
-                cp.penetrationDirection = direction.normalized;
-                cp.penetrationDistance = distance * 1000f;  // in mm
 
-                //save previous
-                cp.previousPenetrationDistance = cp.penetrationDistance;
-                cp.previousPenetrationDirection = cp.penetrationDirection;
-                cp.previousPenetrationVelocity = cp.penetrationVelocity;
-                cp.previousPerpendicularVelocity = cp.perpendicularVelocity;
-                cp.previousPosition = cp.currentPosition;
+                Vector3 direction; float distance; Vector3 velocity;
 
-                //calculate new
-                cp.currentPosition = cp.positionFilter.getValue(surface.transform.position * 1000); // in mm
-                velocity = (cp.currentPosition - cp.previousPosition) / Time.fixedDeltaTime; //in mm/s
-                cp.penetrationVelocity = Vector3.Dot(velocity, cp.penetrationDirection); //in mm/s
-                cp.perpendicularVelocity = cp.velocityFilter.getValue((velocity.magnitude - (cp.penetrationVelocity * cp.penetrationDirection).magnitude)); // in mm/s
+                //interaction calculation
+                if (Physics.ComputePenetration(cp.touchable.touchCollider, cp.touchable.transform.position, cp.touchable.transform.rotation, surface, surface.transform.position, surface.transform.rotation, out direction, out distance)) {
+
+                    //outputs
+                    cp.penetrationDirection = direction.normalized;
+                    cp.penetrationDistance = distance * 1000f;  // in mm
+
+                    //save previous
+                    cp.previousPenetrationDistance = cp.penetrationDistance;
+                    cp.previousPenetrationDirection = cp.penetrationDirection;
+                    cp.previousPenetrationVelocity = cp.penetrationVelocity;
+                    cp.previousPerpendicularVelocity = cp.perpendicularVelocity;
+                    cp.previousPosition = cp.currentPosition;
+
+                    //calculate new
+                    cp.currentPosition = cp.positionFilter.getValue(surface.transform.position * 1000); // in mm
+                    velocity = (cp.currentPosition - cp.previousPosition) / Time.fixedDeltaTime; //in mm/s
+                    cp.penetrationVelocity = Vector3.Dot(velocity, cp.penetrationDirection); //in mm/s
+                    cp.perpendicularVelocity = cp.velocityFilter.getValue((velocity.magnitude - (cp.penetrationVelocity * cp.penetrationDirection).magnitude)); // in mm/s
+                }
+
+                SignalData sd = cp.touchable.onContact(cp);
+
+                //Equalization
+                if (doEqualization) {
+                    if (sd.sineAmplitude > 0) Mathf.Clamp01(sd.sineAmplitude *= equalization.Evaluate(sd.sineFrequency));
+                    if (sd.sawAmplitude > 0) Mathf.Clamp01(sd.sawAmplitude *= equalization.Evaluate(sd.sawFrequency));
+                    if (sd.squareAmplitude > 0) Mathf.Clamp01(sd.squareAmplitude *= equalization.Evaluate(sd.squareFrequency));
+                }
+                signalGenerator.loadPreset(sd);
+
+                //UI updates
+                if (outputUI != null) outputUI.text.text = Mathf.RoundToInt(signalGenerator.signal.sineAmplitude * 100f) + "%";
+
             }
-
-            SignalData sd = cp.touchable.onContact(cp);
-
-            //Equalization
-            if (doEqualization) {
-                if (sd.sineAmplitude > 0) Mathf.Clamp01(sd.sineAmplitude *= equalization.Evaluate(sd.sineFrequency));
-                if (sd.sawAmplitude > 0) Mathf.Clamp01(sd.sawAmplitude *= equalization.Evaluate(sd.sawFrequency));
-                if (sd.squareAmplitude > 0) Mathf.Clamp01(sd.squareAmplitude *= equalization.Evaluate(sd.squareFrequency));
-            }
-            signalGenerator.loadPreset(sd);
-
-            //UI updates
-            if (outputUI != null) outputUI.text.text = Mathf.RoundToInt(signalGenerator.signal.sineAmplitude * 100f) + "%";
-
-        }
-    }
-/*
-    private void OnTriggerStay(Collider other) {
-        int id = other.GetInstanceID();
-        if (!activeContacts.ContainsKey(id)) return;
-        ContactParameters cp = activeContacts[id];
-
-        Vector3 direction; float distance; Vector3 velocity;
-
-        //interaction calculation
-        if (Physics.ComputePenetration(cp.touchable.touchCollider, cp.touchable.transform.position, cp.touchable.transform.rotation, surface, surface.transform.position, surface.transform.rotation, out direction, out distance)) {
-
-            //outputs
-            cp.penetrationDirection = direction.normalized;
-            cp.penetrationDistance = distance * 1000f;  // in mm
-
-            //save previous
-            cp.previousPenetrationDistance = cp.penetrationDistance;
-            cp.previousPenetrationDirection = cp.penetrationDirection;
-            cp.previousPenetrationVelocity = cp.penetrationVelocity;
-            cp.previousPerpendicularVelocity = cp.perpendicularVelocity;
-            cp.previousPosition = cp.currentPosition;
-
-            //calculate new
-            cp.currentPosition = cp.positionFilter.getValue(surface.transform.position * 1000); // in mm
-            velocity = (cp.currentPosition - cp.previousPosition) / Time.fixedDeltaTime; //in mm/s
-            cp.penetrationVelocity = Vector3.Dot(velocity, cp.penetrationDirection); //in mm/s
-            cp.perpendicularVelocity = cp.velocityFilter.getValue((velocity.magnitude - (cp.penetrationVelocity * cp.penetrationDirection).magnitude)); // in mm/s
-
         }
 
-        //movable update
-        if (cp.touchable.objectSettings != null) cp.touchable.objectSettings.doMove(cp, cp.touchable);
-        
-
-        //signal generation
-        SignalData si = cp.touchable.materialSettings.getSignal(cp);
-        signalGenerator.loadPreset(si);
-
-        //UI updates
-        if (outputUI != null) outputUI.text.text = Mathf.RoundToInt(signalGenerator.signal.sineAmplitude * 100f) + "%";
-
-        Debug.Log("OnTriggerStay");
-
+        foreach (int id in lost) {
+            activeContacts[id].touchable.onContactEnd();
+            activeContacts.Remove(id);
+            triggerFilters.Remove(id);
+        }
+        if(lost.Count > 0) exitCleanup();
     }
 
-*/
 
     private void OnTriggerExit(Collider other) {
         int id = other.GetInstanceID();
         if (activeContacts.ContainsKey(id)) {
-            activeContacts[id].touchable.onContactEnd();
-            activeContacts.Remove(id);
-            exitCleanup();
+
+            activeContacts[id].collidesDuringFrame = false;
+            //activeContacts[id].touchable.onContactEnd();
+            //activeContacts.Remove(id);
+            //exitCleanup();
         }
         
     }
@@ -182,6 +163,7 @@ public class StimulationOutput : MonoBehaviour {
             this.stimulationOutput = so;
         }
 
+        public bool collidesDuringFrame = true;
         //instances
         public Touchable touchable;
         public StimulationOutput stimulationOutput;
